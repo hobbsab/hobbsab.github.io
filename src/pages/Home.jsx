5bfc207f61e39fb78e6e3be58d88500eb647dc55
Anna <kayzerscake@gmail.com>
2024-04-23 15:58:18 -0400

    first

---
+++ src/pages/Home.jsx
@@ -0,0 +1,77 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
+        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
+        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
+        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
+        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
+        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
+        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
+        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
+        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
+        fames ac ante ipsum primis in faucibus.
+      </p>
+    </div>
+    "use client";
+
+    import { Footer } from "flowbite-react";
+    import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
+    
+    export function Component() {
+      return (
+        <Footer container>
+          <div className="w-full">
+            <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+              <div>
+                <Footer.Brand
+                  href="https://flowbite.com"
+                  src="https://flowbite.com/docs/images/logo.svg"
+                  alt="Flowbite Logo"
+                  name="Flowbite"
+                />
+              </div>
+              <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+                <div>
+                  <Footer.Title title="about" />
+                  <Footer.LinkGroup col>
+                    <Footer.Link href="#">Flowbite</Footer.Link>
+                    <Footer.Link href="#">Tailwind CSS</Footer.Link>
+                  </Footer.LinkGroup>
+                </div>
+                <div>
+                  <Footer.Title title="Follow us" />
+                  <Footer.LinkGroup col>
+                    <Footer.Link href="#">Github</Footer.Link>
+                    <Footer.Link href="#">Discord</Footer.Link>
+                  </Footer.LinkGroup>
+                </div>
+                <div>
+                  <Footer.Title title="Legal" />
+                  <Footer.LinkGroup col>
+                    <Footer.Link href="#">Privacy Policy</Footer.Link>
+                    <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+                  </Footer.LinkGroup>
+                </div>
+              </div>
+            </div>
+            <Footer.Divider />
+            <div className="w-full sm:flex sm:items-center sm:justify-between">
+              <Footer.Copyright href="#" by="Flowbite™" year={2022} />
+              <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+                <Footer.Icon href="#" icon={BsFacebook} />
+                <Footer.Icon href="#" icon={BsInstagram} />
+                <Footer.Icon href="#" icon={BsTwitter} />
+                <Footer.Icon href="#" icon={BsGithub} />
+                <Footer.Icon href="#" icon={BsDribbble} />
+              </div>
+            </div>
+          </div>
+        </Footer>
+      );
+    }
+  );
+}
+
