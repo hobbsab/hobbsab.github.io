21eb2b9068927bf276329e05f110c474eb6689aa
Anna <kayzerscake@gmail.com>
2024-04-28 20:13:49 -0400

    description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,16 +7,8 @@ export default function Home() {
       <h4>Full-Stack Web Development</h4>
       <img className="rounded-circle mainphoto" alt="avatar" src={profileImg} width="200" height="auto"/>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
+      I'm Anna Hobbs and I'm a full-stack web developer based in North Carolina. 
+      With a passion for creating and designing beautiful and functional web applications, I'm dedicated to creating responsive and user-friendly web solutions that meet your needs.
       </p>
     </div>
   );
