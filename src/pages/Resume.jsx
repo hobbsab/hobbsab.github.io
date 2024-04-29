2813c4e62adc61545fc7de19605c55e62aa2056f
Anna <kayzerscake@gmail.com>
2024-04-28 20:16:18 -0400

    resume page updated

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,6 +5,11 @@ export default function Resume() {
       <h1>Resume / stack skillset</h1>
       <img className= "errorImg" src={htmlcssjs} />
       <p>
+      I have experience in HTML, CSS, JS, JSON, React+Vite, Express.js, MERN Stack and more.
+      Languages spoken: English, German, Swedish, Russian.
+
+      Resume: In Progress
+
         Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
         Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
         mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
